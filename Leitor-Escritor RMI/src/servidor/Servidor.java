69d6c29d618b0d9652c925fb6f2e03593462e0a4
Jorge da Silva <jorge.dasilvajunior@gmail.com>
2014-11-10 22:41:28 -0200

    Commit para Branch da versão com Semaforo

--- Leitor-Escritor RMI/src/servidor/Servidor.java
+++ Leitor-Escritor RMI/src/servidor/Servidor.java
@@ -1,10 +1,3 @@
-/**
- * 
- */
-
-
-
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -20,155 +13,188 @@ import java.util.Properties;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Luiz Felipe
  *
  */
 public class Servidor implements LeitorEscritorRemoteInterface {
-    File file ;
+
+    File file;
     String caminho;
     FileWriter fw;
     FileReader fr;
- 
+
     Properties prop = new Properties();
+    public static Semaphore mutex = new Semaphore(1);
+    public static Semaphore w = new Semaphore(1);
 
-	/**
-	 * indica o numero de leitores correntes*/
-	static int leitores =0;
-	/**
-	 * indica se algum escritor esta escrevendo no momento */
-	static boolean escrevendo = false;
-	
-	/**
-	 * verifica se nao tem nenhum leitor e 
-	 * se nao tem nunnhum escrito acessando
-	 * o arquivo
-	 * @return true se pode escrever false caso contrario
-	 */
-	static boolean condicaoEscrita(){
-		return leitores == 0 && escrevendo==false;
-	}
-	
-	/**
-	 * verifica a condicao de leitura
-	 * @return true caso possa ler
-	 * @return false caso exista um escritor acessando o arquivo
-	 */
-	static boolean condicaoLeitura(){
-		return escrevendo==false;
-	}
+    /**
+     * indica o numero de leitores correntes
+     */
+    static int leitores = 0;
+    /**
+     * indica se algum escritor esta escrevendo no momento
+     */
+    static boolean escrevendo = false;
 
-	/**
-	 * 
-	 */
-	public Servidor() {
-		try {
-			System.out.println("localizando arquivo...");
-			prop.load(Servidor.class.getClassLoader().getResourceAsStream("config.properties"));
-			caminho = prop.getProperty("dir");
-			System.out.println("abrindo arquivo...");
-			file = new File(caminho);
-                        fw = new FileWriter(file);
-                        
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    /**
+     * verifica se nao tem nenhum leitor e se nao tem nunnhum escrito acessando
+     * o arquivo
+     *
+     * @return true se pode escrever false caso contrario
+     */
+    static boolean condicaoEscrita() {
+        return leitores == 0 && escrevendo == false;
+    }
 
-	/* (non-Javadoc)
-	 * @see servidor.LeitorEscritorRemoteInterface#escrever(java.lang.String)
-	 */
-	@Override
-	public void escrever(int id) throws RemoteException {
-	
-		 try {
-			comecaEscrever(id);
-			//executa escrita
-		System.out.println("cliente "+id+" escrevendo no arquivo...");
-			fw = new FileWriter(file);
-	        BufferedWriter bw = new BufferedWriter(fw);
-                
-                bw.append("cliente: "+id +"\n");
-	        bw.append(String.valueOf(Math.random()));
-                bw.newLine();
-	        bw.close();
-	        fw.close();
-		System.out.println("cliente "+id+" fim da escrita");
-			terminaEscrever(id);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    /**
+     * verifica a condicao de leitura
+     *
+     * @return true caso possa ler
+     * @return false caso exista um escritor acessando o arquivo
+     */
+    static boolean condicaoLeitura() {
+        return escrevendo == false;
+    }
 
-	}
+    /**
+     *
+     */
+    public Servidor() {
+        try {
+            System.out.println("localizando arquivo...");
+            prop.load(Servidor.class.getClassLoader().getResourceAsStream("config.properties"));
+            caminho = prop.getProperty("dir");
+            System.out.println("abrindo arquivo...");
+            file = new File(caminho);
+            fw = new FileWriter(file);
 
-	private void comecaEscrever(int id) {
-		while(Controlador.condicaoEscrita() == false){
-			System.out.println("cliente "+id+" aguardando para condicao de escrita...");
-		}
-		Controlador.escrevendo = true;
-		
-	}
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	private void terminaEscrever(int id) {
-		Controlador.escrevendo = false;		
-		System.out.println("cliente "+id+" terminou a escrita...");
-	}
+    /* (non-Javadoc)
+     * @see servidor.LeitorEscritorRemoteInterface#escrever(java.lang.String)
+     */
+    @Override
+    public void escrever(int id) throws RemoteException {
 
-	/* (non-Javadoc)
-	 * @see servidor.LeitorEscritorRemoteInterface#ler()
-	 */
-	@Override
-	public void ler(int id) throws RemoteException {
-		
-		String l = "vazia";
-		try {
-			comecaLer(id);
-			System.out.println("clinte "+id+" lendo arquivo...");
-			//	executa leitura do arquivo
-			fr = new FileReader(file);
-			BufferedReader buffRead = new BufferedReader(fr);
-			while(l != null){
-				l = buffRead.readLine();
-				System.out.println(l);
-			}			
-			terminaLer(id);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException ex){ex.printStackTrace();}
+        try {
+            w.acquire();
+            comecaEscrever(id);
+            //executa escrita
+            System.out.println("cliente " + id + " escrevendo no arquivo...");
+            fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
 
+            bw.append("cliente: " + id + "\n");
+            bw.append(String.valueOf(Math.random()));
+            bw.newLine();
+            bw.close();
+            fw.close();
+            System.out.println("cliente " + id + " fim da escrita");
+            terminaEscrever(id);
+            w.release();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-	}
+    }
 
-	private void terminaLer(int id) {
-		Controlador.leitores--;
-		System.out.println("cliente "+id+" terminou de ler.");
-		
-	}
+    private void comecaEscrever(int id) {
+        while (Controlador.condicaoEscrita() == false) {
+            System.out.println("cliente " + id + " aguardando para condicao de escrita...");
+        }
+        Controlador.escrevendo = true;
 
-	private void comecaLer(int id) {
-		while(Controlador.condicaoLeitura()==false){
-			System.out.println("cliente "+id+" aguardando para condicao de leitura...");
-		}
-		Controlador.leitores++;
-		
-	}
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		/**
-		 * Security Manager para proteger o sistema de acesso
-		 * nao autorizado a recursos desnecessarios. Garante que operacoes
-		 * realiazadas pelo codigo baixado, estao sujeitas a uma politica de 
-		 * seguranca
-		 */
-		
-		if (System.getSecurityManager() == null) {
+    private void terminaEscrever(int id) {
+        Controlador.escrevendo = false;
+        System.out.println("cliente " + id + " terminou a escrita...");
+    }
+
+    /* (non-Javadoc)
+     * @see servidor.LeitorEscritorRemoteInterface#ler()
+     */
+    @Override
+    public void ler(int id) throws RemoteException {
+
+        String l = "vazia";
+        try {
+
+            mutex.acquire();
+
+            leitores = leitores++;
+            if (leitores == 1) {
+                w.acquire();
+            }
+
+            mutex.release();
+            comecaLer(id);
+            System.out.println("clinte " + id + " lendo arquivo...");
+            //	executa leitura do arquivo
+            fr = new FileReader(file);
+            BufferedReader buffRead = new BufferedReader(fr);
+            while (l != null) {
+                l = buffRead.readLine();
+                System.out.println(l);
+            }
+            terminaLer(id);
+
+            mutex.acquire(); 
+
+            leitores = leitores--; 
+            if (leitores == 0) {
+                w.release();
+            }
+            mutex.release();
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private void terminaLer(int id) {
+        Controlador.leitores--;
+        System.out.println("cliente " + id + " terminou de ler.");
+
+    }
+
+    private void comecaLer(int id) {
+        while (Controlador.condicaoLeitura() == false) {
+            System.out.println("cliente " + id + " aguardando para condicao de leitura...");
+        }
+        Controlador.leitores++;
+
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        /**
+         * Security Manager para proteger o sistema de acesso nao autorizado a
+         * recursos desnecessarios. Garante que operacoes realiazadas pelo
+         * codigo baixado, estao sujeitas a uma politica de seguranca
+         */
+
+        if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
         try {
@@ -178,8 +204,8 @@ public class Servidor implements LeitorEscritorRemoteInterface {
              * exporta o objeto para que ele possa receber chamadas remotas.
              * retorna um stub
              */
-            LeitorEscritorRemoteInterface stub =
-                (LeitorEscritorRemoteInterface) UnicastRemoteObject.exportObject(sv, 0);
+            LeitorEscritorRemoteInterface stub
+                    = (LeitorEscritorRemoteInterface) UnicastRemoteObject.exportObject(sv, 0);
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind(name, stub);
             System.out.println("Servidor pronto!");
@@ -188,6 +214,6 @@ public class Servidor implements LeitorEscritorRemoteInterface {
             e.printStackTrace();
         }
 
-	}
+    }
 
 }
